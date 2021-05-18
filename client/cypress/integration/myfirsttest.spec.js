4aff7affcf3d609d522bef7605152a4613e77ea9
Jonas De Corte <jonas.decorte@student.hogent.be>
2021-05-18 14:33:29 +0200

    testing

--- client/cypress/integration/myfirsttest.spec.js
+++ client/cypress/integration/myfirsttest.spec.js
@@ -1,36 +1,76 @@
-const { Button } = require("selenium-webdriver");
-
 describe('My First Test', function() {
   beforeEach(function () {
     cy.login();
   });
     it('our app runs', function() {
       cy.visit('http://localhost:4200');
-      cy.get('[data-cy=filterInput]').type('');
-      cy.get('[data-cy=personeelCard]').should('have.length', );
+      cy.get('[data-cy=personeelCard]').should('have.length', 9);
     });
+ });
+ describe('test add person', function() {
+  beforeEach(function () {
+    cy.login();
   });
-  it('mock personeel get', function() {
-    cy.server({ delay: 1000 });
-    cy.route({
-      method: 'GET',
-      url: '/api/Personeel/',
-      status: 200,
-      response: 'fixture:personeel.json'
+    it('test add person', function() {
+      cy.visit('http://localhost:4200/personeel/add');
+      cy.get('[data-cy=naam]').type('hello world');
+      cy.get('[data-cy=functie]').type('hello world');
+      cy.get('[data-cy=email]').type('helloworld@gmail.com');
+      cy.get('[data-cy=telefoonNummer]').type('0478194517');
+      cy.get('[data-cy=geboorteDatum]').type('2000-05-04');
+      cy.get('[data-cy=datumInDienst]').type('2018-05-04');
+      cy.get('[data-cy=btnsubmit]').click();
+      cy.get('[data-cy=personeelCard]').should('have.length', 10);
     });
-
+ });
+ describe('test delete  person', function() {
+  beforeEach(function () {
+    cy.login();
+  });
+ it('delete while showing', () => {
+  cy.server();
+    // check we have two before we start
     cy.visit('/');
+    cy.get('[data-cy=personeelCard]').should('have.length', 10);
+    // click the second delete button
+    cy.get('[data-cy=personeelList').find('[data-cy=delete]').eq(9).click();
+    // the newly added one should be deleted (and our view automatically updated)
     cy.get('[data-cy=personeelCard]').should('have.length', 9);
   });
-  it('on error should show error message', function() {
+});
+
+describe('personeel List tests', function () {
+  beforeEach(function () {
+    cy.login();
+  });
+  it('delayed response brings state out of sync', () => {
     cy.server();
     cy.route({
       method: 'GET',
-      url: '/api/Personeel/',
-      status: 500,
-      response: 'ERROR'
+      url: '/api/Personeel',
+      status: 200,
+      response: 'fixture:personeel.json',
     });
-    cy.visit('/');
-    cy.get('[data-cy=appError]').should('be.visible');
-  });
+    cy.route({
+      delay: 2000,
+      method: 'GET',
+      url: '/api/Personeel/?name=2test',
+      status: 200,
+      response: 'fixture:2test.json',
+    }).as('get2TestPersoneel');
+    cy.route({
+      method: 'GET',
+      url: '/api/Personeel/?name=3test',
+      status: 200,
+      response: 'fixture:3test.json',
+    }).as('get3TestPersoneel');
 
+    cy.visit('/');
+    cy.get('[data-cy=filterInput]').type('2test');
+    cy.wait(300);
+    cy.get('[data-cy=filterInput]').type('{backspace}{backspace}{backspace}{backspace}{backspace}3test');
+    cy.wait(['@get2TestPersoneel', '@get3TestPersoneel']);
+    cy.get('[data-cy=personeelCard]').should('have.length', 1);
+    cy.get('[data-cy=personeel-title]').should('contain', '3test');
+  });
+});
--- client/cypress/support/commands.js
+++ client/cypress/support/commands.js
@@ -32,3 +32,10 @@ Cypress.Commands.add('login', () => {
       body: { email, password: 'P@ssword1111' },
     }).then((res) => localStorage.setItem('currentUser', res.body));
   });
+  Cypress.Commands.add('register', (email,password) => {
+      cy.request({
+      method: 'POST',
+      url: '/api/account/register',
+      body: { email, password },
+    }).then((res) => localStorage.setItem('currentUser', res.body));
+  });
